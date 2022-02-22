bae6a1877b7fb5992991b9f213ed000d8ba55728
Sophie Arana <arana.sophie@gmail.com>
2022-02-22 14:04:49 +0000

    reading out performance on first transfer trial

--- scripts/seqlearn_analyse_onlinedata.py
+++ scripts/seqlearn_analyse_onlinedata.py
@@ -16,6 +16,9 @@ trial_data.keys()
 experiment_data = data[1]['edata']
 experiment_data.keys()
 
+parameters_data = data[2]['parameters']
+parameters_data.keys()
+
 # %%
 # print basic information (prolific ID, experimental group, time it took to finish)
 #print(experiment_data['expt_turker'])
@@ -23,7 +26,6 @@ experiment_data.keys()
 #print('%i minutes' % np.round((experiment_data['exp_finishtime'] - experiment_data['exp_starttime'])/1000/60,decimals=0))
 
 #%%
-
 # compute accuracy per block
 
 trial_correct = trial_data['resp_correct'][1:] # first response is in instructions
@@ -34,3 +36,16 @@ for iblock, block_data in enumerate(blocked_correct):
     print('{0} accuracy in block {1}'.format(acc, iblock))
 
 #%%
+# compute if performance on 1st possible generalisable item
+
+# find which trial was known based on generalised data.
+last_block = parameters_data['block']['trialorder'][-1]
+
+all_trialid = np.unique(last_block)
+seen_trialid = []
+for itrial, trialid in enumerate(last_block):
+    seen_trialid.append(trialid)
+    if np.array_equal(np.unique(seen_trialid), all_trialid):
+        break
+# correct or not?
+print(blocked_correct[-1][itrial])
