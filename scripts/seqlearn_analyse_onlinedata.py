b15392490a3b60a3f122eb532fb514ece06c684d
Sophie Arana <arana.sophie@gmail.com>
2022-03-31 16:59:45 +0100

    cleaned up analysis code

--- scripts/seqlearn_analyse_onlinedata.py
+++ scripts/seqlearn_analyse_onlinedata.py
@@ -8,84 +8,105 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# %%
-def check_submission(df, filename):
-        iloc = df_per_subject.index[df_per_subject['filename']== filename].tolist()[0]
-        #print out info from servers side
-        print('Subject ID ' + df['filename'][iloc])
-        print('Prolific ID ' + df['participant_id'][iloc])
-        print('Group ' + df['condition'][iloc])
-        print('Time to finish: %.2f minutes'% np.round(df['duration'][iloc], decimals=2))
-        print('Prolific time recorded: {0}'.format(np.round(df['time_taken'][iloc]/60)))
-        try:
-            print('Time spend on instructions: %.2f'% np.round(df['instruction_time'][iloc], decimals=2))
-        except Exception:
-            print('Time spend on instructions: unkown')
+# %% general helper functions
+def retrieve_data(path_to_file, strlist):
+    """ Load in data saved from javascript online study
+        Deals with missing '{}' and separates out saved keys
+        into variables
 
-        #breaks taken
-        for iblock, duration in enumerate(df['all_pause'][iloc]):
-            print('spend {0} minutes on pause {1}'.format(np.round(duration,decimals=2),iblock))
-        print('## Debrief ##')
-        for field in df['debrief'][iloc]:
-            print(field)
+    Parameters:
+        path_to_file (str) : full path to file that contains data
+        strlist (list) : list of strings with dict names to extract from files
 
-        #print out info from prolific side
-        try:
-            print('## Prolific ##')
-            print('Country of Birth: ' + df['Country of Birth'][iloc])
-            print('Age: {0}'.format(df['age'][iloc]))
-        except Exception:
-            print('no prolific data')
+    Returns:
+        out (list) : list of dicts from file
 
-strlist = ['sdata','edata','parameters']
-filename = os.path.join(path_to_data,'fF8yti4iKr78.txt')
-def retrieve_data(filename, strlist):
-    out  = []
-    data = []
-    with open(filename, "r") as f:
-        data = f.read().split('\n')
-    data = [json.loads('{' + line + '}') for line in data]
+    """
 
-    for i, keyword in enumerate(strlist):
-        out.append(data[i][keyword])
+    out = []
+    data_read = []
+    with open(path_to_file, "r") as file:
+        data_read = file.read().split('\n')
+    data_read = [json.loads('{' + line + '}') for line in data_read]
+
+    for i_dict, keyword in enumerate(strlist):
+        out.append(data_read[i_dict][keyword])
 
     return out
 
 # compute accuracy per block
-def compute_bonus(responses,max_bonus):
-    bonus_var = 0
-    for iblock, block_data in enumerate(responses):
-        acc = np.nansum(block_data)/len(block_data)
-        pay = (((acc*100)-50)/50)*(max_bonus/5)
-        bonus_var = bonus_var+pay
+def compute_bonus(responses, max_bonus):
+    """ Computes total bonus earned based on series of responses labeled as
+        correct/incorrect and given the maximum amount possible to achieve,
+        assuming chance level at 50%
+        and bonus to increase with each additional percent correct
+
+    Parameters:
+        responses (list) : list of correct or incorrect labels per trials
+        max_bonus (int) : amount of bonus to earn given 100% accuracy
+
+    Return:
+        bonus_var (int) : amount of bonus earned given accuracy across responses
+
+    """
+    acc = np.nansum(responses)/len(responses)
+    bonus_var = (((acc*100)-50)/50)*max_bonus
+
     return bonus_var
 
 # find which trial was known based on generalised data.
-def retrieve_uniqueness_point(blocked_trialorder):
-    all_trialid = np.unique(blocked_trialorder[0])
-    uniqueness_point = []
-    for trialorder in blocked_trialorder:
-        seen_trialid = []
-        for itrial, trialid in enumerate(trialorder):
-            if np.array_equal(np.unique(seen_trialid), all_trialid):
-                break
-            seen_trialid.append(trialid)
-        uniqueness_point.append(itrial)
-    return uniqueness_point
+def retrieve_uniqueness_point(seq_id):
+    """ Finds index of first repeating sequence
+        after all unique sequences have been shown at least once.
+        (This is the latest trial at which a rule should be uniquely identified)
 
-def map_rule2ruleid(input_array):
-    if 3 in input_array[0]:
-        idx_rule = 'simple'
-    else:
-        idx_rule = 'complex'
+    Parameters:
+        seq_id (list) : list of sequence ID for each trial in order of presentation
+    Return:
+        i_trial (int) : index (trial number)
 
-    rule_names = np.array(['Input', 'X', 'state','None'])
+    """
+    all_trialid = np.unique(seq_id)
+    seen_trialid = []
+    i_trial = None
+    for i_trial, trialid in enumerate(seq_id):
+        if np.array_equal(np.unique(seen_trialid), all_trialid):
+            break
+        seen_trialid.append(trialid)
 
-    name_rule = [rule_names[i] for i in input_array]
-
-    return idx_rule, name_rule
+    return i_trial
 
 #%%
+## PRINT A BUNCH OF INFO TO GET A FEEL FOR HOW PARTICIPANT DID
+
+def output_submission_details(df, fname):
+    iloc = df_subject.index[df_subject['filename'] == fname].tolist()[0]
+    #print out info from servers side
+    print('Subject ID ' + df['filename'][iloc])
+    print('Prolific ID ' + df['participant_id'][iloc])
+    print('Group ' + df['condition'][iloc])
+    print('Prolific time recorded: {0}'.format(np.round(df['time_taken'][iloc]/60)))
+    print('Time to finish: %.2f minutes'% np.round(df['duration'][iloc], decimals=2))
+    try:
+        print('Time spend on instructions: %.2f'% np.round(df['instruction_time'][iloc], decimals=2))
+    except TypeError:
+        print('Time spend on instructions: unkown')
+
+        #breaks taken
+    for i_block, duration in enumerate(df['all_pause'][iloc]):
+        print('spend {0} minutes on pause {1}'.format(np.round(duration, decimals=2), i_block))
+    print('## Debrief ##')
+    for field in df['debrief'][iloc]:
+        print(field)
+
+        #print out info from prolific side
+    try:
+        print('## Prolific ##')
+        print('Country of Birth: ' + df['Country of Birth'][iloc])
+        print('Age: {0}'.format(df['age'][iloc]))
+    except Exception:
+        print('no prolific data')
+
 ## COLLECT DEMOGRAPHIC DATA & CHECK FOR X-STUDY DUPLICATES
 def fetch_demographics(path_to_demographic, path_to_data):
 
@@ -109,29 +130,24 @@ def fetch_demographics(path_to_demographic, path_to_data):
         if not os.path.isfile(f):
             continue
 
-        [_, experiment_data,parameters_data] = retrieve_data(f,['sdata','edata','parameters'])
+        [t_data, e_data,parameters_data] = retrieve_data(f,['sdata','edata','parameters'])
 
-        this_csv = all_csvs.loc[all_csvs['participant_id'] == experiment_data['expt_turker']]
+        this_csv = all_csvs.loc[all_csvs['participant_id'] == e_data['expt_turker']]
         df_out = df_out.append(this_csv, ignore_index=True)
         all_files.append(file_name)
-        all_rules.append(experiment_data['expt_group'])
+        all_rules.append(e_data['expt_group'])
 
-        try:
-            all_instruct_time.append((trial_data['resp_timestamp'][1] - experiment_data['exp_starttime'])/1000/60)
-            all_duration.append((experiment_data['exp_finishtime'] - experiment_data['exp_starttime'])/1000/60)
-            pause_time = []
-            for iblock, start_time in enumerate(experiment_data['block_starttime']):
-                pause_time.append((experiment_data['block_finishtime'][iblock] - start_time)/1000/60)
-            all_pause.append(pause_time)
-        except Exception:
-            all_instruct_time.append(None)
-            all_duration.append(None)
-            all_pause.append(None)
+        all_instruct_time.append((t_data['resp_timestamp'][1] - e_data['exp_starttime'])/1000/60)
+        all_duration.append((e_data['exp_finishtime'] - e_data['exp_starttime'])/1000/60)
+        pause_time = []
+        for iblock, start_time in enumerate(e_data['block_starttime']):
+            pause_time.append((e_data['block_finishtime'][iblock] - start_time)/1000/60)
+        all_pause.append(pause_time)
 
-        debrief_fields = [x for x in experiment_data.keys() if x.startswith('debrief')]
+        debrief_fields = [x for x in e_data.keys() if x.startswith('debrief')]
         tmp_debrief = []
         for field in debrief_fields:
-            tmp_debrief.append(field + ' - ' +  experiment_data[field])
+            tmp_debrief.append(field + ' - ' +  e_data[field])
         all_debrief.append(tmp_debrief)
 
     df_out['filename'] = all_files
@@ -144,35 +160,38 @@ def fetch_demographics(path_to_demographic, path_to_data):
     return df_out
 
 ## COLLECT BEHAVIORAL DATA
-def fetch_data(path_to_data,list_of_filenames):
-    df_out = pd.DataFrame(columns=['i_subject','group',
-                                     'block_num', 'trial_num', 'rule','seq','correct', 'rt'])
+def fetch_data(path_to_data, list_of_filenames):
+    df_out = pd.DataFrame(columns=['i_subject', 'group', 'block_num',
+                                    'trial_num', 'rule', 'seqid',
+                                    'correct', 'rt'])
 
     for isub, file_name in enumerate(list_of_filenames):
         #get participant data
         f = os.path.join(path_to_data, file_name)
-        [trial_data, experiment_data,parameters_data] = retrieve_data(f,['sdata','edata','parameters'])
+        [trial_data, experiment_data, parameters_data] = retrieve_data(f,['sdata','edata','parameters'])
 
         #Check if participants saw 1-step test block
         if len(parameters_data['block']['trialorder'][-1]) < len(parameters_data['block']['trialorder'][-2]):
             #last block has fewer trials
             test_block = True
 
-        for itrial in trial_data['expt_index']:
-            if itrial == None:
+        for index in trial_data['expt_index']:
+            if index == None:
                 continue
 
-            iblock = trial_data['expt_block'][itrial]
+            iblock = trial_data['expt_block'][index]
+            itrial = trial_data['expt_trial'][index]
 
             row = pd.Series([
                 file_name,
                 experiment_data['expt_group'],
                 iblock,
-                trial_data['expt_trial'][itrial],
+                trial_data['expt_trial'][index],
                 parameters_data['ruleid'][iblock],
-                trial_data['seq'][itrial],
-                trial_data['resp_correct'][itrial],
-                trial_data['resp_reactiontime'][itrial]
+                #trial_data['seq'][index],
+                parameters_data['block']['trialorder'][iblock][itrial-1],
+                trial_data['resp_correct'][index],
+                trial_data['resp_reactiontime'][index]
             ], index=df_out.columns)
 
             df_out = df_out.append(row, ignore_index=True)
@@ -180,62 +199,91 @@ def fetch_data(path_to_data,list_of_filenames):
     return df_out
 
 #%%
-path_to_data = "data/prolific/data/2step_long"
-path_to_demographic = "data/prolific/demographics"
+path_data = "data/prolific/data/2step_v1"
+path_demographic = "data/prolific/demographics"
 
-df_per_subject = fetch_demographics(path_to_demographic, path_to_data)
-df_per_subject[df_per_subject['condition']=='simple']
-df_data = fetch_data(path_to_data, df_per_subject['filename'].values)
+df_subject = fetch_demographics(path_demographic, path_data)
+df_data = fetch_data(path_data, df_subject['filename'].values)
 
-df_acc = df_data[['i_subject','block_num','correct']].groupby(['i_subject','block_num']).agg(['sum','count'])
+#compute accuracy
+df_acc = df_data[['i_subject', 'block_num', 'correct']].groupby(['i_subject', 'block_num']).agg(['sum', 'count'])
 df_acc.columns = ['_'.join(column) for column in df_acc.columns]
 df_acc['acc'] = df_acc['correct_sum']/df_acc['correct_count']
 df_acc = df_acc['acc'].unstack()
+filename = 'ZySjMhKVBPgI.txt'
+for filename in  df_subject['filename'].values:
+    start_date = df_subject[df_subject['filename'] == filename]['started_datetime']
+    #if not start_date.str.startswith('2022-03-30').values[0]:
+    #    continue
 
-for filename in  df_per_subject['filename'].values:
-    if not df_per_subject[df_per_subject['filename']==filename]['started_datetime'].str.startswith('2022-03-30').values[0]:
-        continue
     print(filename)
-    check_submission(df_per_subject, filename)
+    output_submission_details(df_subject, filename)
     print('PERFORMANCE: ')
     for iblock in range(len(df_acc.columns)):
-        print(np.round(df_acc[iblock][filename],decimals=2))
+        print(np.round(df_acc[iblock][filename], decimals=2))
+
+#%% Visualize trial order, why did some people learn so well?
+plt.figure(figsize=(30, 10))
+for filename in  df_subject['filename'].values:
+    dt_tmp = df_data[df_data['i_subject']==filename]
+    acc = dt_tmp[dt_tmp['block_num']==2]['correct'].mean()
+    if acc > 0.6:
+        mycolor = 'r'
+    else: mycolor = 'k'
+    f = os.path.join(path_data, filename)
+    [_, _, parameters_data] = retrieve_data(f, ['sdata','edata','parameters'])
+    trials = parameters_data['block']['trialorder']
+    trials_arr = list(range(len(trials)))
+    plt.plot(trials_arr, trials,lw=2,color=mycolor)
+    plt.title(filename)
 
 #%% First pass, output info for each participant, this is to quickly check whether data quality is okay
-#FIXME: double check that figures are being created correctly
-#FIXME: double check that people (specifically those with smaller blocks) are seeing correct sequence - output combinations
-# create some figures to show Chris
-#FIXME: renalayse old data with new pipeline. Double check if trial order was highly biased in earlier pilots
-for file_name in df_per_subject['filename'].values:
-    [trial_data, experiment_data,parameters_data] = retrieve_data(os.path.join(path_to_data,file_name),['sdata','edata','parameters'])
-    #Visualize performance
-    trial_duration = parameters_data['timing']['seqduration']/1000
-    num_timeouts = len([i for i in range(len(trial_data['resp_reactiontime'])) if trial_data['resp_reactiontime'][i] == None])
 
-    single_data = df_data[df_data['i_subject']==file_name]
+for file_name in df_subject['filename'].values:
+    path_file = os.path.join(path_data, file_name)
+
+    #extract some variables from the data
+    [s_data, e_data, p_data] = retrieve_data(path_file, ['sdata', 'edata', 'parameters'])
+    trial_duration = p_data['timing']['seqduration']/1000
+
+    single_data = df_data[df_data['i_subject'] == file_name]
+
+    n_trials_presented = len(single_data)
     n_block = len(np.unique(single_data['block_num']))
-    fig = plt.figure(figsize=(15, 10))
+
+    #Visualize performance
+
+    fig = plt.figure(figsize=(30, 15))
     iblock = 0
     for iblock in np.unique(single_data['block_num']):
-        blocked_rt = single_data[single_data['block_num']==iblock]['rt']
-        blocked_correct = single_data[single_data['block_num']==iblock]['correct']
+        if iblock == 11:
+            break
+        blocked_rt = single_data[single_data['block_num'] == iblock]['rt']
+        idx_timeouts = [i for i, value in enumerate(blocked_rt) if value is None]
+        blocked_correct = single_data[single_data['block_num'] == iblock]['correct']
+        idx_incorrect = np.where(blocked_correct != 1)
+        idx_unique = retrieve_uniqueness_point(single_data['seqid'])
+
         trials_arr = list(range(len(blocked_rt)))
-        ymin = 0#np.min(block_data)
-        ymax = 10#np.max(block_data)
+        # set time-outs to 0 RT and subtract duration of trial from response counter
+        blocked_rt = [0 if i is None else i-trial_duration for i in blocked_rt]
+        y_min = np.min(blocked_rt)
+        y_max = np.max(blocked_rt)
+
+        idx_incorrect = [i for i in idx_incorrect[0] if i not in idx_timeouts]
+
         # plot RTs
         plt.subplot(n_block, 1, iblock+1)
-        plt.plot(trials_arr, blocked_rt.tolist())
-        # mark incorrect trials
-        idx_incorrect = np.where(blocked_correct != 1)
-        plt.vlines(idx_incorrect, ymin, ymax, 'k')
-        # mark first trial that can be inferred through completion based on the other trials
-        #FIXMEplt.vlines(unique_ids[iblock], ymin, ymax, 'r')
+        plt.plot(trials_arr, blocked_rt)
+        plt.vlines(idx_incorrect, y_min, y_max, 'k')
+        plt.vlines(idx_timeouts, y_min, y_max, 'r')
+        plt.vlines(idx_unique, y_min, y_max, 'b')
 
-        plt.ylim(ymin,ymax)
-    fig.legend(['reaction time', 'incorrect trials'], loc='lower center')
+        plt.ylim(y_min,y_max)
+    fig.legend(['reaction time', 'incorrect trials', 'time_out', 'uniqueness point'], loc='lower center')
     fig.suptitle('subject {0} - group {1}'.format(set(single_data['i_subject']), set(single_data['group'])))
     plt.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9)
-    #plt.savefig('results/prolific/'+'RT_' + file_name[:-4] + '.jpg')
+    plt.savefig('results/prolific/2step_short/'+'summaryfig_' + file_name[:-4] + '.jpg')
 
 
 #bonus_sheet = pd.DataFrame({'id':all_names, 'pay':all_bonus})
@@ -417,6 +465,3 @@ group1 = df_data.where((df_data.group == 'complex') & (df_data.block_num == 'blo
 group2 = df_data.where((df_data.group== 'simple') & (df_data.block_num == 'block_5')).dropna()
 # add the output to the dictionary
 test_results['main'] = mannwhitneyu(group1['acc_corrected'],group2['acc_corrected'])
-
-
-#%%
