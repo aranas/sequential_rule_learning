171dc316a19798a279cf6964ec848fb349bc76c5
Sophie Arana <arana.sophie@gmail.com>
2022-07-17 17:51:11 +0100

    reject data based on median absolute deviation

--- scripts/seqlearn_plotting.py
+++ scripts/seqlearn_plotting.py
@@ -10,8 +10,12 @@ from scipy.stats import mannwhitneyu
 import src.behavior_preprocess as prep
 
 #%%
-PATH_DATA = "data/v3/preprocessed"
+PATH_DATA = "data/v4/preprocessed"
 
+NB_BLOCK_FAR = 14 #index of far transfer block
+NB_BLOCK_1STEP = 13
+
+#%%
 with open(''.join([PATH_DATA, '/all_data', '_csv']), 'rb') as file:
     all_data = pickle.load(file)
 
@@ -20,13 +24,13 @@ n_blocks = len(all_data['expt_block'].unique())
 
 # fix condition naming; exclude single step block; exclude practice trial (block == nan)
 all_data.loc[all_data['expt_group'] == 'simple', 'expt_curriculum'] = 'simple_blocked_magician'
-all_data = all_data[all_data.expt_block != 9]
+all_data = all_data[all_data.expt_block != NB_BLOCK_1STEP]
 all_data = all_data[~all_data['expt_block'].isnull()]
+
 #%% subselect data
-#cond1 = (all_data.expt_group == 'simple') & (all_data.expt_curriculum == 'blocked_input')
-cond2 = (all_data.expt_group == 'simple')
-cond3 = (all_data.expt_group == 'complex') & (all_data.expt_curriculum == 'interleaved')
-all_data = all_data.loc[cond2 | cond3]
+#cond2 = (all_data.expt_group == 'simple')
+#cond3 = (all_data.expt_group == 'complex') & (all_data.expt_curriculum == 'interleaved')
+#all_data = all_data.loc[cond2 | cond3]
 
 cols_plot = ['expt_group', 'expt_block', 'expt_curriculum', 'expt_turker']
 df_acc = all_data.groupby(cols_plot, as_index=False)['resp_correct'].apply(lambda x: (np.nansum(x)/len(x))*100)
@@ -36,7 +40,6 @@ df_acc = pd.DataFrame(df_acc)
 #%%
 fig = plt.figure(figsize=(20, 10))
 g = sns.lineplot(x='expt_block', y='resp_correct',
-                 style='generaliser',
                  hue='expt_curriculum',
                  data=df_acc,
                  err_style='bars', err_kws={'capsize':6}, marker='o', ci=95)
@@ -47,7 +50,7 @@ fig = plt.figure(figsize=(10, 10))
 ax = sns.swarmplot(x='expt_block', y='resp_correct', hue='expt_curriculum', data=df_acc)
 plt.legend(bbox_to_anchor=(1.02, 0.55), loc='upper left', borderaxespad=0)
 
-plt.savefig('results/prolific/v3/main_acc_complex_leavvsinput.jpg', bbox_inches="tight")
+#plt.savefig('results/prolific/v3/main_acc_complex_leavvsinput.jpg', bbox_inches="tight")
 
 #%% Mann-Whitney-Test
 all_data['expt_curriculum'].unique()
@@ -60,35 +63,62 @@ for i_block in all_data['expt_block'].unique():
     test_results[str(i_block)] = mannwhitneyu(group1['resp_correct'], group2['resp_correct'])
 
 #%% Exclude participants
+
+# exclude for tool use
+all_data = all_data[~all_data['debrief_tools'].str.contains('yes')]
+
 timeout_thres = 10
 n_many_timeouts = all_data[all_data['n_timeout'] >= timeout_thres]['expt_subject'].nunique()
 print('exclude {0} - too many time-outs'.format(n_many_timeouts))
 all_data = all_data[all_data['n_timeout'] <= timeout_thres]
 
+
+
 # include only ppl that learned
-min_train_thresh = 0.9
-n_no_learning =all_data[all_data['max_training_score']<min_train_thresh]['expt_turker'].nunique()
-print('exclude {0} - not learning throughout blocks '.format(n_no_learning))
+#min_train_thresh = 0.9
+#n_no_learning =all_data[all_data['max_training_score']<min_train_thresh]['expt_turker'].nunique()
+#print('exclude {0} - not learning throughout blocks '.format(n_no_learning))
 #all_data = all_data[all_data['max_training_score']>min_train_thresh]
+
+#exclude participants pt 2 - nonlearners
+def median_absolute_deviation(x):
+    B = 1.4826
+    mad = B * np.median(np.absolute(x - np.median(x)))
+    threshold = np.median(x) - (2*mad)
+    return threshold
+
+cols_plot = ['expt_group', 'expt_block', 'expt_curriculum', 'expt_turker']
+df_acc = all_data.groupby(cols_plot, as_index=False)['resp_correct'].apply(lambda x: (np.nansum(x)/len(x))*100)
+df_acc = pd.DataFrame(df_acc)
+df_mad_thresh = df_acc.groupby(['expt_curriculum', 'expt_block'], as_index=False)['resp_correct'].apply(
+        lambda x: median_absolute_deviation(x))
+
+df_acc.loc[df_acc.expt_curriculum=='simple_blocked_magician'].hist(column='resp_correct')
+
+idx_late_blocks = [1.0,3.0,5.0,7.0]
+all_data['learner'] = None
+for i,row in all_data.iterrows():
+    sub_acc = df_acc.loc[(df_acc.expt_turker==row['expt_turker']) & (df_acc.expt_block.isin(idx_late_blocks))]
+    mad = df_mad_thresh.loc[(df_mad_thresh.expt_curriculum==row['expt_curriculum']) &  (df_mad_thresh.expt_block.isin(idx_late_blocks))]
+
+    all_data.loc[[i]]['learner'] = not np.any(np.array(sub_acc['resp_correct']) < np.array(mad['resp_correct']))
+
 
 col_group = ['expt_group', 'expt_curriculum', 'expt_block']
 all_data.groupby(col_group)['expt_subject'].nunique()
 
+col_group = ['expt_group', 'expt_curriculum', 'learner']
+all_data.groupby(col_group)['expt_subject'].nunique()
 
 #%% Plot individual subject data (RT, time-outs, moving average)
 # select data
-col_group = ['expt_turker','expt_index']
-np_acc = prep.pd2np(all_data, col_group, 'resp_correct')
-nsubj, n_ttrials = np_acc.shape
 
-#np_rt = prep.pd2np(all_data, col_group, 'resp_reactiontime')
-#nsubj, n_ttrials = np_rt.shape
+col_group = ['expt_turker','expt_index']
+np_acc = prep.pd2np(all_data, col_group, 'resp_correct') #or: 'resp_correct''resp_reactiontime'
+nsubj, n_ttrials = np_acc.shape
 
 # fetch modalities per participant
 modalities = all_data[['expt_turker', 'expt_curriculum']].drop_duplicates()['expt_curriculum'].to_numpy()
-len(modalities)
-
-max_score = all_data[['expt_turker', 'max_training_score']].drop_duplicates()['max_training_score'].to_numpy()
 len(modalities)
 
 #moving average (to capture progression within blocks as well)
@@ -96,21 +126,15 @@ window_size = 32
 stride = 32
 mov_avg = np.squeeze([[np.nanmean(np_acc[:,i:i+window_size],axis=1)]for i in range(0, n_ttrials, stride)
                    if i+window_size <= n_ttrials]).T
-mov_avg.shape
 
 #fig = plt.figure(figsize=(10,10))
 #plt.imshow(mov_avg.T)
 uniq_modalities = list(set(modalities))
-
 fig, axs = plt.subplots(len(uniq_modalities), 1, figsize=(15, 12), dpi=300, facecolor='w')
 for i, (ax, modality) in enumerate(zip(axs, uniq_modalities)):
 
     # Select subjects for this modality
-    idx_mod = modality == modalities
-    idx_ceil = max_score > min_train_thresh
-    idx_low = max_score < min_train_thresh
-
-    idx = idx_mod & idx_low
+    idx = modality == modalities
 
     m_subj = mov_avg[idx,:]
     m = np.nanmean(m_subj, axis=0)
@@ -141,7 +165,7 @@ for i, (ax, modality) in enumerate(zip(axs, uniq_modalities)):
         )
 
     # plot second group
-    idx = idx_mod & idx_ceil
+    idx = idx_mod
 
     m_subj = mov_avg[idx,:]
     m = np.nanmean(m_subj, axis=0)
@@ -177,7 +201,7 @@ for i, (ax, modality) in enumerate(zip(axs, uniq_modalities)):
     ax.set_ylabel('Accuracy')
     ax.set_xticks(center_idx)
     ax.set_xticklabels(center_idx*32)
-    #ax.set_ylim(5, 7) #for RTs
+    ax.set_ylim(5, 7) #for RTs
 
     ax.axvline(1.5, color='k', ls='-', alpha=.4)
     ax.axvline(3.5, color='k', ls='-', alpha=.4)
@@ -189,9 +213,8 @@ for i, (ax, modality) in enumerate(zip(axs, uniq_modalities)):
     ax.axhline(1/2., color='k', ls='--', alpha=.4)
 
 plt.tight_layout()
-plt.savefig('results/prolific/v3/learners_split2.jpg', bbox_inches="tight")
+#plt.savefig('results/prolific/v3/learners_split2.jpg', bbox_inches="tight")
 
-#%%
 
 #%% Mann-Whitney-Test
 idx_ceil = max_score > min_train_thresh
