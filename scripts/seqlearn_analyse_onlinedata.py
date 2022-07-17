4c435df25d780553daded14a968e4036bf40b807
Sophie Arana <arana.sophie@gmail.com>
2022-07-17 17:50:13 +0100

    linting

--- scripts/seqlearn_analyse_onlinedata.py
+++ scripts/seqlearn_analyse_onlinedata.py
@@ -2,26 +2,25 @@
 
 #%%
 import os
+from collections import Counter
 import pickle
 import numpy as np
 import pandas as pd
-from collections import Counter
-import matplotlib.pyplot as plt
 import src.behavior_preprocess as prep
 
 #%% SCRIPT PARAMETERS
-PATH_DATA = "data/v3/data"
+PATH_DATA = "data/v4"
 PATH_DEMOGRAPHIC = "data/prolific/demographics"
-PATH_RESULTS = "data/v3/preprocessed"
+PATH_RESULTS = "data/v4/preprocessed"
 
-iblock_far = 10 #index of far transfer block
-
+NB_BLOCK_FAR = 14 #index of far transfer block
+NB_BLOCK_1STEP = 13
 
 #%%GET ALL DATA INTO DATAFRAME
 
 df_subject = prep.fetch_demographics(PATH_DEMOGRAPHIC, PATH_DATA)
 
-with open('data_log.txt', "r") as file:
+with open('data_log.txt', "r", encoding="utf8") as file:
     log = file.read().split('\n')
 df_log = pd.DataFrame([i.split(' ')[1:] for i in log])
 
@@ -58,7 +57,7 @@ submissions = all_data['expt_subject'].unique()
 subjs = all_data['expt_turker'].unique()
 counts = Counter(all_data['expt_turker'])
 
-with open('data_log.txt', "r") as file:
+with open('data_log.txt', "r", encoding="utf8") as file:
     log = file.read().split('\n')
 df_log = pd.DataFrame([i.split(' ')[1:] for i in log])
 
@@ -72,7 +71,7 @@ if len(subjs) < len(submissions):
                 idx_match = df_log.iloc[:, -1] == file+'.txt'
                 time = df_log[idx_match].iloc[:, -2].values
                 day = df_log[idx_match].iloc[:, -3].values
-                print('{0} completed at {1} on {2}'.format(file, time[0], day[0]))
+                print(f'{file} completed at {time[0]} on {day[0]}')
 
 #%%
 ##############################################
@@ -86,48 +85,54 @@ for subj in all_subj:
     prep.output_submission_details(all_data, subj)
 
 #%% Save bonus to file
-with open('bonus.csv', 'w') as out:
-    datasel = all_data[(all_data.expt_group == 'simple') & (all_data.expt_curriculum == 'semi-interleaved')]
+with open('bonus.csv', 'w', encoding="utf8") as out:
+    datasel = all_data[(all_data.expt_group == 'complex') & (all_data.expt_curriculum == 'blocked_magician')]
     for subj, subj_data in datasel.groupby('expt_subject'):
         #if int(df_log[df_log.iloc[:, -1] == subj + '.txt'][9].values[0]) < 29:
         #    continue
         prolific_id = subj_data['expt_turker'].unique()
         bonus_vec = np.array(subj_data['block_bonus'].iloc[0])
         bonus_vec = bonus_vec[bonus_vec != np.array(None)]
-        bonus = sum([bonus for bonus in bonus_vec if bonus > 0])
+        bonus = np.round(sum([bonus for bonus in bonus_vec if bonus > 0]), decimals=2)
 
         group = subj_data['expt_group'].unique()[0]
         curr = subj_data['expt_curriculum'].unique()[0]
         subid = subj_data['expt_subject'].unique()[0]
-        out.write('{0},{1}\n'.format(prolific_id[0], bonus, group, curr, subid))
+        out.write(f'{prolific_id[0]},{bonus}\n')
         #out.write('{4} {2}-{3} {0},{1}\n'.format(subj, bonus, group, curr, subid))
 
 #%% Preprocessing
+
 # add info time-outs
-timeouts = all_data.groupby(['expt_turker'], as_index=False)['resp_correct'].apply(lambda x: pd.Series({'n_timeout':
-                                                                               x.isnull().sum()}))
+timeouts = all_data.groupby(['expt_turker'], as_index=False)['resp_correct'].apply(
+    lambda x: pd.Series({'n_timeout':x.isnull().sum()}))
+
 all_data = pd.merge(all_data,timeouts, on='expt_turker')
 
-df_acc = all_data.groupby(['expt_turker', 'expt_block'], as_index=False)['resp_correct'].apply(lambda x: pd.Series({'acc':
-                                                                x.sum()/x.count()}))
+df_acc = all_data.groupby(['expt_turker', 'expt_block'], as_index=False)['resp_correct'].apply(
+    lambda x: pd.Series({'acc':x.sum()/x.count()}))
+
 # add info on far transfer
 df_acc['generaliser'] = False
-for subj in df_acc.loc[(df_acc.acc > 0.6) & (df_acc.expt_block == iblock_far),'expt_turker']:
+for subj in df_acc.loc[(df_acc.acc > 0.6) & (df_acc.expt_block == NB_BLOCK_FAR),'expt_turker']:
     df_acc.loc[df_acc.expt_turker == subj,'generaliser'] = True
-all_data = pd.merge(all_data,df_acc.loc[df_acc.expt_block==iblock_far][['expt_turker','generaliser']], on='expt_turker')
+all_data = pd.merge(all_data,df_acc.loc[df_acc.expt_block==NB_BLOCK_FAR][['expt_turker','generaliser']],
+                     on='expt_turker')
 
 #add info any learning
-df_learner = df_acc.groupby(['expt_turker'], as_index=False).apply(lambda x: pd.Series({'max_training_score': np.max(x['acc'].values[:7]) }))
+df_learner = df_acc.groupby(['expt_turker'], as_index=False).apply(
+            lambda x: pd.Series({'max_training_score': np.max(x['acc'].values[:7]) }))
 all_data = pd.merge(all_data,df_learner, on='expt_turker')
 
 #add info 1-step rule
-step1_acc = all_data.loc[(all_data.expt_block==9) & (all_data.rule==3)].groupby(['expt_turker'], as_index=False)['resp_correct'].apply(
+step1_acc = all_data.loc[(all_data.expt_block==NB_BLOCK_1STEP) & (all_data.rule==3)].groupby(
+            ['expt_turker'], as_index=False)['resp_correct'].apply(
             lambda x: pd.Series({'ruleF_acc': np.nanmean(x) }))
 all_data = pd.merge(all_data, step1_acc, on='expt_turker')
-step1_acc = all_data.loc[(all_data.expt_block==9) & (all_data.rule==5)].groupby(['expt_turker'], as_index=False)['resp_correct'].apply(
+step1_acc = all_data.loc[(all_data.expt_block==NB_BLOCK_1STEP) & (all_data.rule==5)].groupby(
+            ['expt_turker'], as_index=False)['resp_correct'].apply(
             lambda x: pd.Series({'ruleF_acc': np.nanmean(x) }))
 all_data = pd.merge(all_data, step1_acc, on='expt_turker')
-
 
 #%% Save data
 
